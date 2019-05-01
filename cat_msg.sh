0ae352c07c33706635ea59443f47f047ff9d6225
tarosaiba <taro.skywalker1117@gmail.com>
2019-05-01 12:27:47 +0900

    Initial commit

---
+++ cat_msg.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+if [ ! $# -eq 2 ]; then
+    echo "usage: $0 <broker_host> <topic_name>"
+    exit 1;
+fi
+
+BROKER=$1
+TOPIC=$2
+
+docker run --interactive --rm \
+       confluentinc/cp-kafkacat \
+       kafkacat -b ${BROKER}:9092 \
+       -t ${TOPIC} \
+       -P -C\
+       -f 'Key (%K bytes): %k\tValue (%S bytes): %s\tPartition: %p\tOffset: %o\n)'
